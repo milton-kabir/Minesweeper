f743b02d3da7a53240333748216be53e8b52353c
milton-kabir <milton.cse.pust@gmail.com>
2021-07-25 18:28:26 +0600

    Third Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -6,18 +6,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+
         // write your code here
-        String ar[] = new String[9];
-        String st = ".........";
-        for (int i = 0; i < 9; i++) {
+        String ar[] = new String[13];
+        String st = ".............";
+        for (int i = 0; i < 13; i++) {
             ar[i] = st;
         }
-        int cc = 0;
-        int[] a = {0,1,2,3,4,5,6,7,8};
-        int[] b = {0,1,2,3,4,5,6,7,8};
+        int[] a = {9,10,2,3,4,5,6,7,8};
+        int[] b = {9,10,2,3,4,5,6,7,8};
         List<int[]> list = new ArrayList<>();
         for (int i = 0; i < a.length; ++i)
             for (int j = 0; j < b.length; ++j)
@@ -31,12 +30,50 @@ public class Main {
         for (int i = 0; i < cnt; i++) {
             int x=list.get(i)[0];
             int y=list.get(i)[1];
-
             ar[x]=ar[x].substring(0,y)+"X"+ar[x].substring(y+1);
         }
-        for (int i = 0; i < 9; i++) {
-            System.out.println(ar[i]);
+        for (int i = 2; i <=10; i++) {
+//            System.out.println(ar[i]);
+            for(int j=2;j<=10;j++){
+                int cc=0;
+                if(ar[i].charAt(j)=='X'){
+                    continue;
+                }
+                if(ar[i].charAt(j+1)=='X'){
+                    cc++;
+                }
+                if(ar[i].charAt(j-1)=='X'){
+                    cc++;
+                }
+                if(ar[i-1].charAt(j)=='X'){
+                    cc++;
+                }
+                if(ar[i-1].charAt(j-1)=='X'){
+                    cc++;
+                }
+                 if(ar[i-1].charAt(j+1)=='X'){
+                    cc++;
+                }
+                 if(ar[i+1].charAt(j)=='X'){
+                    cc++;
+                }
+                 if(ar[i+1].charAt(j-1)=='X'){
+                    cc++;
+                }
+                 if(ar[i+1].charAt(j+1)=='X'){
+                    cc++;
+                }
+                 if(cc==0){
+                     continue;
+                 }
+                ar[i]=ar[i].substring(0,j)+Integer.toString(cc)+ar[i].substring(j+1);
+            }
         }
-
+        for(int i=2;i<=10;i++){
+            for(int j=2;j<=10;j++){
+                System.out.print(ar[i].charAt(j));
+            }
+            System.out.println();
+        }
     }
 }
