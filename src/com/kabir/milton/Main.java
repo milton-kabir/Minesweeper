3ecef56de46656111f62d7c3bc7c80062c92d12c
milton-kabir <milton.cse.pust@gmail.com>
2021-07-25 16:23:08 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,19 @@
+//package minesweeper;
+
 package com.kabir.milton;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(".X.......\n" +
+                ".....X..X\n" +
+                "....X....\n" +
+                "......X..\n" +
+                "..X......\n" +
+                "....X....\n" +
+                "..X......\n" +
+                "..X......\n" +
+                "......X..");
     }
 }
