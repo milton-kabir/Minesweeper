6f6db11ac55a2c083ee5b4ec564403f00746f5b8
milton-kabir <milton.cse.pust@gmail.com>
2021-07-25 22:07:44 +0600

    Finally Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,144 +1,300 @@
 //package minesweeper;
 
-package com.kabir.milton;
+ package com.kabir.milton;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+
+class Cell {
+    boolean isMine = false;
+    boolean isMarked = false;
+    boolean isOpened = false;
+
+    int row;
+    int col;
+
+}
+
+class Game {
+    Field field;
+    Result result = new Result(this);
+    int markedMines = 0;
+    boolean isFirstTurn = true;
+    Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        System.out.print("How many mines do you want on the field? ");
+        int mines = scanner.nextInt();
+        field = new Field(mines);
+        field.printField();
+
+        boolean isExploded = false;
+
+        while (result.isNotGameOver()) {
+            System.out.print("Set/unset mines marks or claim a cell as free: ");
+            int col = Integer.parseInt(scanner.next()) - 1;
+            int row = Integer.parseInt(scanner.next()) - 1;
+            boolean commandMarkFree = scanner.next().equals("free");
+
+            if (commandMarkFree) {
+                if (isFirstTurn) {
+                    field.checkFirstTurn(row, col);
+                    isFirstTurn = false;
+                }
+                isExploded = result.checkExplodes(field.cells[row][col]);
+                if (isExploded) {
+                    field.printField();
+                    break;
+                } else {
+                    field.openArea(row, col);
+                }
+            } else {
+                markedMines += field.markCellAsMine(field.cells[row][col]);
+            }
+            field.printField();
+        }
+        if (isExploded) {
+            System.out.println("You stepped on a mine and failed!");
+        } else {
+            System.out.println("Congratulations! You found all mines!");
+        }
+    }
+}
+
+class Result {
+    Game game;
+
+    public Result(Game game) {
+        this.game = game;
+    }
+
+
+    public boolean isAllMarkedMinesFound() {
+        boolean isAllMinesMarked = true;
+
+        for (Cell mine : game.field.minesArray) {
+            if (!mine.isMarked) {
+                isAllMinesMarked = false;
+                break;
+            }
+        }
+
+        boolean isMinesQuantity = game.markedMines == game.field.minesArray.size();
+        return isAllMinesMarked && isMinesQuantity;
+    }
+
+    public boolean isNoOpenMinesLeft() {
+        for (int i = 0; i < game.field.cells.length; i++) {
+            for (int j = 0; j < game.field.cells.length; j++) {
+                if (!game.field.cells[i][j].isOpened && !game.field.cells[i][j].isMine) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isNotGameOver() {
+        return !(isAllMarkedMinesFound() || isNoOpenMinesLeft());
+    }
+
+    public boolean checkExplodes(Cell cell) {
+        if (cell.isMine) {
+            game.field.setAllBombOpened();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Field {
+    final int SIZE = 9;
+    final char UNKNOWN = '.';
+    final char EMPTY = '/';
+    final char MINE = 'X';
+    final char MARKED = '*';
+
+    Cell[][] cells;
+    List<Cell> minesArray = new ArrayList<>();
+    Stack<Cell> stack = new Stack<>();
+
+    public Field(int mines) {
+        cells = new Cell[SIZE][SIZE];
+        int minesQuantity = 0;
+
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells.length; j++) {
+                cells[i][j] = new Cell();
+                cells[i][j].row = i;
+                cells[i][j].col = j;
+            }
+        }
+
+        while (minesQuantity < mines) {
+            Random random = new Random();
+            int row = random.nextInt(SIZE);
+            int col = random.nextInt(SIZE);
+            if (!cells[row][col].isMine) {
+                cells[row][col].isMine = true;
+                minesArray.add(cells[row][col]);
+                minesQuantity++;
+            }
+        }
+    }
+
+    public void printField() {
+        System.out.println(" │123456789│\n—│—————————│");
+        for (int i = 0; i < cells.length; i++) {
+            System.out.print(i + 1 + "|");
+            for (int j = 0; j < cells.length; j++) {
+                if (cells[i][j].isOpened) {
+                    if (cells[i][j].isMine) {
+                        System.out.print(MINE);
+                    } else {
+                        int numberOfMine = checkMineAround(i, j);
+                        if (numberOfMine == 0) {
+                            System.out.print(EMPTY);
+                        } else {
+                            System.out.print(numberOfMine);
+                        }
+                    }
+                } else if (cells[i][j].isMarked) {
+                    System.out.print(MARKED);
+                } else {
+                    System.out.print(UNKNOWN);
+                }
+            }
+            System.out.println("|");
+        }
+        System.out.println("—│—————————│");
+    }
+
+    public void checkFirstTurn(int row, int col) {
+        if (cells[row][col].isMine) {
+            for (int i = 0; i < (SIZE * SIZE); i++) {
+                int findingRow = i / cells.length;
+                int findingCol = i % cells.length;
+                if (findingRow != row || findingCol != col) {
+                    if (!cells[findingRow][findingCol].isMine) {
+                        cells[findingRow][findingCol].isMine = true;
+                        cells[row][col].isMine = false;
+                        minesArray.remove(cells[row][col]);
+                        minesArray.add(cells[findingRow][findingCol]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public int checkMineAround(int row, int col) {
+        int result = 0;
+
+        if (cells[row][col].isMine) {
+            return -1;
+        }
+
+        int upShift = 1;
+        int downShift = 1;
+        int leftShift = 1;
+        int rightShift = 1;
+
+        if (row == 0) {
+            upShift = 0;
+        }
+        if (row == cells.length - 1) {
+            downShift = 0;
+        }
+        if (col == 0) {
+            leftShift = 0;
+        }
+        if (col == cells.length - 1) {
+            rightShift = 0;
+        }
+        for (int i = row - upShift; i <= row + downShift; i++) {
+            for (int j = col - leftShift; j <= col + rightShift; j++) {
+                if (cells[i][j].isMine) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public void checkAllAround(int row, int col) {
+        int upShift = 1;
+        int downShift = 1;
+        int leftShift = 1;
+        int rightShift = 1;
+
+        if (row == 0) {
+            upShift = 0;
+        }
+        if (row == cells.length - 1) {
+            downShift = 0;
+        }
+        if (col == 0) {
+            leftShift = 0;
+        }
+        if (col == cells.length - 1) {
+            rightShift = 0;
+        }
+        for (int i = row - upShift; i <= row + downShift; i++) {
+            for (int j = col - leftShift; j <= col + rightShift; j++) {
+                if (!cells[i][j].isOpened) {
+                    if (checkMineAround(i, j) == 0) {
+                        cells[i][j].isOpened = true;
+                        stack.push(cells[i][j]);
+
+                    } else if (checkMineAround(i, j) > 0) {
+                        cells[i][j].isOpened = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void openArea(int row, int col) {
+        if (checkMineAround(row, col) > 0) {
+            cells[row][col].isOpened = true;
+        } else {
+            cells[row][col].isOpened = true;
+            checkAllAround(row, col);
+
+            while (!stack.empty()) {
+                Cell nextCell = stack.pop();
+                checkAllAround(nextCell.row, nextCell.col);
+            }
+        }
+    }
+
+    public void setAllBombOpened() {
+        for (Cell each : minesArray) {
+            each.isOpened = true;
+        }
+    }
+
+    public int markCellAsMine(Cell cell) {
+        if (!cell.isOpened) {
+            if (!cell.isMarked) {
+                cell.isMarked = true;
+                return 1;
+            } else {
+                cell.isMarked = false;
+                return -1;
+            }
+        } else {
+            System.out.println("There is a number here!");
+            return 0;
+        }
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-
         // write your code here
-        String ar[] = new String[12];
-        String br[] = new String[12];
-        String st = "-|.........|";
-        for (int i = 0; i < 12; i++) {
-            ar[i] = st;
-        }
-        st="-|---------|";
-        ar[1]=st;
-        ar[11]=st
-;        for (int i = 2; i <= 10; i++) {
-            ar[i] = Integer.toString(i - 1) + ar[i].substring(1);
-            ar[0] = ar[0].substring(0, i) + Integer.toString(i - 1) + ar[0].substring(i + 1);
-        }
-
-        int[] a = {9, 10, 2, 3, 4, 5, 6, 7, 8};
-        int[] b = {9, 10, 2, 3, 4, 5, 6, 7, 8};
-        List<int[]> list = new ArrayList<>();
-        for (int i = 0; i < a.length; ++i)
-            for (int j = 0; j < b.length; ++j)
-                list.add(new int[]{a[i], b[j]});
-
-        Collections.shuffle(list);
-//        System.out.println(list);
-        System.out.println("How many mines do you want on the field?");
-        Scanner sc = new Scanner(System.in);
-        int cnt = sc.nextInt();
-        System.out.println();
-        for (int i = 0; i < cnt; i++) {
-            int x = list.get(i)[0];
-            int y = list.get(i)[1];
-            ar[x] = ar[x].substring(0, y) + "X" + ar[x].substring(y + 1);
-        }
-        for (int i = 2; i <= 10; i++) {
-//            System.out.println(ar[i]);
-            for (int j = 2; j <= 10; j++) {
-                int cc = 0;
-                if (ar[i].charAt(j) == 'X') {
-                    continue;
-                }
-                if (ar[i].charAt(j + 1) == 'X') {
-                    cc++;
-                }
-                if (ar[i].charAt(j - 1) == 'X') {
-                    cc++;
-                }
-                if (ar[i - 1].charAt(j) == 'X') {
-                    cc++;
-                }
-                if (ar[i - 1].charAt(j - 1) == 'X') {
-                    cc++;
-                }
-                if (ar[i - 1].charAt(j + 1) == 'X') {
-                    cc++;
-                }
-                if (ar[i + 1].charAt(j) == 'X') {
-                    cc++;
-                }
-                if (ar[i + 1].charAt(j - 1) == 'X') {
-                    cc++;
-                }
-                if (ar[i + 1].charAt(j + 1) == 'X') {
-                    cc++;
-                }
-                if (cc == 0) {
-                    continue;
-                }
-                ar[i] = ar[i].substring(0, j) + Integer.toString(cc) + ar[i].substring(j + 1);
-            }
-        }
-        for (int i = 0; i < br.length; i++) {
-            br[i] = "";
-            for (int j = 0; j < ar[i].length(); j++) {
-                br[i] += ar[i].charAt(j);
-            }
-        }
-//        for (int i = 0; i < 12; i++) {
-//            System.out.println(br[i]);
-//        }
-//        System.out.println("----------------");
-//        System.out.println("----------------");
-//        System.out.println("----------------");
-        for (int i = 2; i <= 10; i++) {
-            for (int j = 2; j <= 10; j++) {
-                if (ar[i].charAt(j) == 'X') {
-                    ar[i] = ar[i].substring(0, j) + "." + ar[i].substring(j + 1);
-                }
-            }
-        }
-        for (int i = 0; i < 12; i++) {
-            System.out.println(ar[i]);
-        }
-
-        int cnt2 = 0, mrk = 0;
-        while (true) {
-            System.out.println("Set/delete mines marks (x and y coordinates):");
-            int xx = sc.nextInt();
-            int yy = sc.nextInt();
-            xx++;
-            yy++;
-            if (ar[yy].charAt(xx) == '.') {
-                if(br[yy].charAt(xx)=='X'){
-                    cnt2++;
-                }
-                ar[yy] = ar[yy].substring(0, xx) + "*" + ar[yy].substring(xx + 1);
-                mrk++;
-                for (int i = 0; i < 12; i++) {
-                    System.out.println(ar[i]);
-                }
-                if(cnt2==cnt&&mrk==cnt){
-                    System.out.println("Congratulations! You found all the mines");
-                    break;
-                }
-            }
-            else if (ar[yy].charAt(xx) == '*') {
-                ar[yy] = ar[yy].substring(0, xx) + "." + ar[yy].substring(xx + 1);
-                mrk--;
-                for (int i = 0; i < 12; i++) {
-                    System.out.println(ar[i]);
-                }
-                if(cnt2==cnt&&mrk==cnt){
-                    System.out.println("Congratulations! You found all the mines");
-                    break;
-                }
-            } else {
-                System.out.println("There is a number here!");
-                continue;
-            }
-
-        }
+        Game game = new Game();
+        game.start();
     }
 }
